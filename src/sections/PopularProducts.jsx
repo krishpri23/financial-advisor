4249503ad90a8455bf84fe2142a43d9f8797bd90
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-11-09 10:52:24 -0800

    clear console

--- src/sections/PopularProducts.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function PopularProducts() {
-  return <div>PopularProducts</div>;
-}
-
-export default PopularProducts;
--- src/sections/Subscribe.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Subscribe() {
-  return <div>Subscribe</div>;
-}
-
-export default Subscribe;
